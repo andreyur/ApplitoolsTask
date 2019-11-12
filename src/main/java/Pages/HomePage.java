f45ae50ecb1402cc388471a411d00b1f60f0d9d3
Andrey <aulaul@mail.ru>
2019-11-12 22:15:07 +0200

    Completed functional tests

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -19,19 +19,6 @@ public class HomePage extends BasePage  {
         super(driver);
     }
     public static final String TRANSACTIONS_TABLE = "#transactionsTable";
-    public static final String LOGO = "a.logo[href='/']";
-    public static final String LOGIN_LINK = "a.item[href*='login.html']";
-    public static final String RADIO_BUTTON_TYPE = "[for='buRadioType']";
-    public static final String SELECT_CATEGORIES = "#categories";
-    public static final String BRAND_INPUT = "#brandTooltipBrandAutocompleteInput-brand";
-    public static final String MODEL_INPUT = "#brandTooltipBrandAutocompleteInput-model";
-    public static final String SELECT_YEAR = "#yearFrom";
-    public static final String SEARCH_BUTTON = "[type='submit']";
-
-
-
-
-
 
     @FindBy (css = TRANSACTIONS_TABLE)
     private WebElement transactionsTable;
@@ -39,83 +26,9 @@ public class HomePage extends BasePage  {
         return transactionsTable;
     }
 
-    @FindBy (css = LOGO)
-    private WebElement logo;
-    public WebElement getLogo() {
-        return logo;
-    }
-
-    @FindBy (css = LOGIN_LINK)
-    private WebElement loginLink;
-    public WebElement getLoginLink() {
-        return loginLink;
-    }
-
-    @FindBy (css = RADIO_BUTTON_TYPE)
-    private WebElement radioButtonType;
-    public WebElement getRadioButtonType() {
-        return radioButtonType;
-    }
-
-    @FindBy (css = SELECT_CATEGORIES)
-    private WebElement selectCategories;
-    public WebElement getSelectCategories() {
-        return selectCategories;
-    }
-
-    @FindBy (css = BRAND_INPUT)
-    private WebElement brandInput;
-    public WebElement getBrandInput() {
-        return brandInput;
-    }
-
-    @FindBy (css = MODEL_INPUT)
-    private WebElement modelInput;
-    public WebElement getModelInput() {
-        return modelInput;
-    }
-
-    @FindBy (css = SELECT_YEAR)
-    private WebElement selectYear;
-    public WebElement getSelectYear() {
-        return selectYear;
-    }
-
-    @FindBy (css = SEARCH_BUTTON)
-    private WebElement searchButton;
-    public WebElement getSearchButton() {
-        return searchButton;
-    }
-
     public HomePage open(String url){
         driver.get(url);
         driver.manage().window().maximize();
         return this;
-    }
-
-    public LoginPage clickLoginLink(){
-        loginLink.click();
-        return new LoginPage(driver);
-    }
-
-
-
-
-    public void shouldHaveHost(String host) throws URISyntaxException {
-        assertThat(new URI(driver.getCurrentUrl()).getHost(), equalTo("auto.ria.com"));
-    }
-    public void shouldHaveTitle(String title){
-        assertThat(driver.getTitle(), equalTo(title));
-    }
-
-   // public void shouldHaveSearchForm(){
-       // assertThat("Search Form should be displayed",searchForm.isDisplayed());
-   // }
-
-    public void shouldHaveLogo(){
-        assertThat("Logo should be displayed",logo.isDisplayed());
-    }
-    public void shouldHaveLoginLink(){
-        assertThat("Login link should be displayed",loginLink.isDisplayed());
     }
 }
--- src/test/java/TraditionalTests.java
+++ src/test/java/TraditionalTests.java
@@ -2,9 +2,16 @@ import Pages.*;
 import Utils.BrowserFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.*;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
 import static Pages.BasePage.BASE_URL;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TraditionalTests {
@@ -54,6 +61,62 @@ public class TraditionalTests {
         if(!username.equals("")&& !password.equals("")){
             assertThat("We should see Transactions Table on the Home Page",homePage.getTransactionsTable().isDisplayed());
         }
+    }
+
+    @Test
+    public void testSortTable() throws URISyntaxException {
+        homePage=loginPage.login("username","password");
+        assertThat("We should see Transactions Table on the Home Page",homePage.getTransactionsTable().isDisplayed());
+        int numberRows = (driver.findElements(By.xpath("//tbody/tr"))).size();
+        int numberColumns = (driver.findElements(By.xpath("//tbody/tr[1]/td"))).size();
+        List<List> listRows = new ArrayList<>();
+        for(int i=0;i<numberRows;i++) {
+            int m = i+1;
+            List<String> row = new ArrayList<>();
+            for (WebElement element : driver.findElements(By.xpath("//tbody/tr["+m+"]/td"))) {
+                row.add(element.getText());
+            }
+            listRows.add(row);
+        }
+        driver.findElement(By.cssSelector("th#amount")).click();
+        List<List> listUpdatedRows = new ArrayList<>();
+        for(int i=0;i<numberRows;i++) {
+            int m = i+1;
+            if(i<numberRows-1){
+                int next = m+1;
+                String topRow = driver.findElement(By.xpath("//tbody/tr["+m+"]/td[5]")).getText().split("U")[0].replace(",","");
+                String nextRow = driver.findElement(By.xpath("//tbody/tr["+next+"]/td[5]")).getText().split("U")[0].replace(",","");
+                double topAmount = Double.parseDouble(topRow.substring(1));
+                if(topRow.contains("-")) topAmount = 0-topAmount;
+                double nextAmount = Double.parseDouble(nextRow.substring(1));
+                if(nextRow.contains("-")) nextAmount = 0-nextAmount;
+                assertThat("Top Amount "+topAmount+" is more then Next Amount "+nextAmount,topAmount<nextAmount);
+            }
+            List<String> row = new ArrayList<>();
+            for (WebElement element : driver.findElements(By.xpath("//tbody/tr["+m+"]/td"))) {
+                row.add(element.getText());
+            }
+            String time = driver.findElement(By.xpath("//tbody/tr[" + m + "]/td[2]")).getText();
+            for(int j=0;j<numberRows;j++) {
+                if(time.equals(listRows.get(j).get(1))){
+                    Assert.assertTrue(listRows.get(j).equals(row));
+                }
+            }
+        }
+
+    }
+
+    @Test
+    public void testCanvas() throws URISyntaxException {
+        //Sorry, I am unable to automate canvas
+    }
+
+    @Test
+    public void testDynamicContent() throws URISyntaxException {
+        driver.get(BASE_URL+"?showAd=true");
+        homePage=loginPage.login("username","password");
+        assertThat("First Ad should be displayed", driver.findElement(By.cssSelector("#flashSale img")).isDisplayed());
+        assertThat("Second Ad should be displayed", driver.findElement(By.cssSelector("#flashSale2 img")).isDisplayed());
     }
 
     @DataProvider(name = "Login")
